a12cf9fbc15d54fe44f582656ec9b021c84e6ad0
OlegMalanchuk <126597034+OlegMalanchuk@users.noreply.github.com>
2023-03-30 17:58:50 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,131 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+//        ***
+//        System.out.println("№1");
+//        for (int i = 500; i<=650;i+=10)
+//        {
+//            System.out.println(i);
+//        }
+//        ***
+//        ***
+//        int k = 500;
+//        do {
+//            System.out.println(k);
+//            k+=10;
+//        }while (k<=650);
+//        ***
+
+//        ***
+//        int n = 500;
+//        while (n<=650)
+//        {
+//            System.out.println(n);
+//            n +=10;
+//        }
+//        ***
+
+
+//        ***
+//        System.out.println("№2");
+//        int a = 2 ;int d = 0;
+//        for (int i = 5000; i >= 0  ; i = i - ((2* a++) - 1)) {
+//            System.out.println( ++d +") = " + i );
+//        }
+//        ***
+
+
+//        ***
+//        System.out.println("№3");
+//        double i = 1;
+//        for (int n = 10; i !=11;){
+//            double num = n/i;
+//            i++;
+//            if (n%i==0){
+//                System.out.println(num);}
+//        }
+//        ***
+
+//        ***
+//        System.out.println("№4");
+//        int n = 10; /*введіть число для знах.  його факторіалу  */
+//        int res = 1;
+//        for (int i = 1 ; i <=n; i++)
+//        {
+//             res = res *i;
+//            System.out.println(res);
+//        }
+//        ***
+
+
+
+//        ***
+//        int n = 10;
+//        int res = 1;
+//        int i = 1;
+//        while (i <= n){
+//            res = res *i;
+//            i++;
+//            System.out.println(res);
+//        }
+//        ***
+
+
+
+//        ***
+//        System.out.println("№5");
+//        for (int hour = 0; hour < 24; ) {
+//            hour++;
+//            for (int minut = 0; minut < 60; ) {
+//                minut++;
+//                int c, d, d_c;
+//                c = minut / 10;
+//                d = minut % 10;
+//                d_c = (d * 10) + c;//е для тогоо щоб вихолили числа по типу 24=42, ттіпа вони ж ті самі числа мабть
+//
+//                if (hour == minut) {
+//                    System.out.println(hour + " : " + minut);
+//
+//                } else if (hour == d_c) {
+//                    System.out.println(hour + " : " + minut);
+//
+//                }
+//
+//            }
+//        }
+//
+//        ***
+
+
+//        ***
+//        System.out.println("№6");
+//        int i = 0;
+//        for (int n = 0; i != 1000000; n++) {
+//
+//            int a, b, c, d, e, f;
+//            a = n / 100000;
+//            b = n / 10000 % 10;
+//            c = n / 1000 % 10;
+//            d = n / 100 % 10;
+//            e = n / 10 % 10;
+//            f = n % 10;
+//            int sum_1 = a + b + c;
+//            int sum_2 = d + f + e ;
+//            if (sum_2 == sum_1)
+//            {
+//                System.out.println(" ");
+//                System.out.println(sum_1 + " = " + sum_2);
+//                System.out.println(n);
+//                System.out.println(a+"\n"+b+"\n"+c+"\n"+d+"\n"+e+"\n"+f);
+//            }
+//            i++;
+//        }
+//
+//
+//        ***
+    }
+}
+
+
+
